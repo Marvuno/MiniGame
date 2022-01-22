c9e01f1f0987cda96976ecadd78f827b68209e64
Marvuno <huijunhuan@gmail.com>
2022-01-22 15:19:30 +0800

    added rps

--- main.py
+++ main.py
@@ -7,24 +7,21 @@ from higher_lower_game.higher_lower_main import Higher_Lower_Game
 from quiz_game.quizzler_main import Quizzler_Game
 from snake.snake_main import Snake_Game
 from turtle_race.race_main import Race_Game
+from rps.rps_main import RPS
+
+game_list = {1: Turtle_Crossing_Game,
+             2: Pong_Game,
+             3: Hangman_Game,
+             4: Blackjack_Game,
+             5: Number_Guessing_Game,
+             6: Higher_Lower_Game,
+             7: Quizzler_Game,
+             8: Snake_Game,
+             9: Race_Game,
+             10: RPS}
 
 game_option = int(input("1 for Turtle Crossing, 2 for Ping Pong, 3 for Hangman, 4 for Blackjack, 5 for Number "
-                        "Guessing Game, 6 for Higher Lower Game, 7 for Quiz Game, 8 for Snake, 9 for Turtle Race: "))
-if game_option == 1:
-    Turtle_Crossing_Game()
-elif game_option == 2:
-    Pong_Game()
-elif game_option == 3:
-    Hangman_Game()
-elif game_option == 4:
-    Blackjack_Game()
-elif game_option == 5:
-    Number_Guessing_Game()
-elif game_option == 6:
-    Higher_Lower_Game()
-elif game_option == 7:
-    Quizzler_Game()
-elif game_option == 8:
-    Snake_Game()
-elif game_option == 9:
-    Race_Game()
+                        "Guessing Game, 6 for Higher Lower Game, 7 for Quiz Game, 8 for Snake, 9 for Turtle Race, "
+                        "10 for Rock Paper Scissors: "))
+
+game_list[game_option]()
